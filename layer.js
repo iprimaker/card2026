1873dbc77147fdc5a63d94979ba6dd4e47778207
iprimaker <kinokonoshanv@gmail.com>
2026-07-07 13:56:38 +0900

    Create layer.js

---
+++ layer.js
@@ -0,0 +1,30 @@
+import { getCanvas } from "./canvas.js";
+
+const LAYER_ORDER = {
+    background: 1,
+    character: 2,
+    frame: 3,
+    attribute: 4,
+    text: 5,
+    serial: 6,
+    buzzPower: 7
+};
+
+export function sortLayers(){
+
+    const canvas = getCanvas();
+
+    if(!canvas) return;
+
+    const objects = canvas.getObjects();
+
+    objects.sort((a, b) => {
+        return (LAYER_ORDER[a.layerType] || 0) - (LAYER_ORDER[b.layerType] || 0);
+    });
+
+    objects.forEach((obj, index) => {
+        canvas.moveTo(obj, index);
+    });
+
+    canvas.requestRenderAll();
+}
