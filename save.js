1e0b6d8520451ec35cc9902bf36b3d208cb91105
iprimaker <kinokonoshanv@gmail.com>
2026-07-07 13:57:45 +0900

    Create save.js

---
+++ save.js
@@ -0,0 +1,26 @@
+import { getCanvas } from "./canvas.js";
+import { sortLayers } from "./layer.js";
+
+export function initSave(){
+    const saveButton = document.getElementById("saveButton");
+
+    saveButton.addEventListener("click", saveImage);
+}
+
+function saveImage(){
+    const canvas = getCanvas();
+
+    sortLayers();
+    canvas.discardActiveObject();
+    canvas.renderAll();
+
+    const image = canvas.toDataURL({
+        format: "png",
+        quality: 1
+    });
+
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "aipri-card.png";
+    link.click();
+}
