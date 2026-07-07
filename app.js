5d2d4210e33aab0f38974a1210d3a7c580334a02
iprimaker <kinokonoshanv@gmail.com>
2026-07-07 13:36:49 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,59 @@
+import { initCanvas } from "./canvas.js";
+import { initImages } from "./image.js";
+import { initFrame } from "./frame.js";
+import { initAttribute } from "./attribute.js";
+import { initText } from "./text.js";
+import { initSerial } from "./serial.js";
+import { initSave } from "./save.js";
+import { initReset } from "./reset.js";
+import { initBuzzPower } from "./buzzPower.js";
+import { getCurrentCardType, setCurrentCardType } from "./config.js";
+
+export function startApp(){
+
+    const cardTypeSelect = document.getElementById("cardType");
+    const config = getCurrentCardType();
+
+    cardTypeSelect.value = config.type;
+
+    initCanvas();
+    initImages();
+    initFrame();
+    initAttribute();
+    initText();
+    initSerial();
+    initBuzzPower();
+    initSave();
+    initReset();
+
+    cardTypeSelect.addEventListener("change", () => {
+        setCurrentCardType(cardTypeSelect.value);
+        location.reload();
+    });
+
+    resizePreview();
+
+    console.log("Original Card Maker 起動");
+
+}
+
+window.addEventListener("resize", resizePreview);
+
+function resizePreview(){
+
+    const preview = document.querySelector(".preview");
+    const fabricContainer = document.querySelector(".canvas-container");
+
+    if(!preview || !fabricContainer){
+        return;
+    }
+
+    const scale = Math.min(
+        preview.clientWidth / 697,
+        preview.clientHeight / 1016
+    ) * 0.9;
+
+    fabricContainer.style.transform = `scale(${scale})`;
+    fabricContainer.style.transformOrigin = "center center";
+
+}
