6b143bb2b0dc0c6fac25b8f5995090397fc8fefb
iprimaker <kinokonoshanv@gmail.com>
2026-07-09 02:13:59 +0900

    Update save.js

--- save.js
+++ save.js
@@ -4,46 +4,27 @@ import { sortLayers } from "./layer.js";
 export function initSave(){
     const saveButton = document.getElementById("saveButton");
 
-    if(!saveButton) return;
-
     saveButton.addEventListener("click", saveImage);
 }
 
 function saveImage(){
+
     const canvas = getCanvas();
 
     sortLayers();
     canvas.discardActiveObject();
     canvas.renderAll();
 
-    showSaveToast("保存中...");
+    const image = canvas.toDataURL({
+        format: "png",
+        quality: 1
+    });
 
-    setTimeout(() => {
-        const image = canvas.toDataURL({
-            format: "png",
-            quality: 1
-        });
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "original-card.png";
+    link.click();
 
-        const link = document.createElement("a");
-        link.href = image;
-        link.download = "aipri-card.png";
-        link.click();
-
-        showSaveToast("保存しました！");
-    }, 400);
+    alert("保存しました！");
 }
-
-function showSaveToast(message){
-    const toast = document.getElementById("saveToast");
-    const text = toast.querySelector(".saveText");
-
-    text.textContent = message;
-
-    toast.classList.add("show");
-
-    clearTimeout(toast.hideTimer);
-
-    toast.hideTimer = setTimeout(() => {
-        toast.classList.remove("show");
-    }, 1800);
 }
