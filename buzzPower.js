97c1a6e7635c2ccd232456630b70c7494b6a8b42
iprimaker <kinokonoshanv@gmail.com>
2026-07-09 03:26:53 +0900

    Update buzzPower.js

--- buzzPower.js
+++ buzzPower.js
@@ -12,25 +12,50 @@ const BUZZ_POWER_LIST = {
         { id: "buzz4", name: "1050", path: "./A1050.png" },
         { id: "buzz5", name: "1100", path: "./A1100.png" },
         { id: "buzz6", name: "1150", path: "./A1150.png" },
-        { id: "buzz7", name: "1200", path: "./A1200.png" }, 
+        { id: "buzz7", name: "1200", path: "./A1200.png" },
         { id: "buzz8", name: "1250", path: "./A1250.png" },
         { id: "buzz9", name: "1300", path: "./A1300.png" },
         { id: "buzz10", name: "1320", path: "./A1320.png" },
         { id: "buzz11", name: "1350", path: "./A1350.png" },
         { id: "buzz12", name: "1400", path: "./A1400.png" }
     ],
-    B: [ 
-        { id: "buzz1", name: "只今準備中です", path: "../assets/buzzpower/B900.png" }
-    ]
+
+    B: {
+        frame1: [
+            { id: "buzz1", name: "900", path: "./B_suki_900.png" },
+            { id: "buzz2", name: "950", path: "./B_suki_950.png" },
+            { id: "buzz3", name: "1000", path: "./B_suki_1000.png" }
+        ],
+
+        frame2: [
+            { id: "buzz1", name: "900", path: "./B_yume_900.png" },
+            { id: "buzz2", name: "950", path: "./B_yume_950.png" },
+            { id: "buzz3", name: "1000", path: "./B_yume_1000.png" }
+        ],
+
+        frame3: [
+            { id: "buzz1", name: "900", path: "./B_yuujou_900.png" },
+            { id: "buzz2", name: "950", path: "./B_yuujou_950.png" },
+            { id: "buzz3", name: "1000", path: "./B_yuujou_1000.png" }
+        ]
+    }
 };
 
 export function initBuzzPower(){
 
     const buzzPowerSelect = document.getElementById("buzzPower");
     const buzzPowerArea = document.getElementById("buzzPowerArea");
+    const frameSelect = document.getElementById("frame");
     const config = getCurrentCardType();
 
-    const buzzPowers = BUZZ_POWER_LIST[config.type] || [];
+    let buzzPowers = [];
+
+    if(config.type === "A"){
+        buzzPowers = BUZZ_POWER_LIST.A;
+    }else{
+        const frame = frameSelect.value;
+        buzzPowers = BUZZ_POWER_LIST.B[frame] || [];
+    }
 
     if(buzzPowers.length === 0){
         buzzPowerArea.style.display = "none";
@@ -47,13 +72,19 @@ export function initBuzzPower(){
         buzzPowerSelect.appendChild(option);
     });
 
-    buzzPowerSelect.addEventListener("change", () => {
+    buzzPowerSelect.onchange = () => {
         const selected = buzzPowers.find(buzz => buzz.id === buzzPowerSelect.value);
 
         if(selected){
             drawBuzzPower(selected.path);
         }
-    });
+    };
+
+    if(config.type === "B"){
+        frameSelect.onchange = () => {
+            initBuzzPower();
+        };
+    }
 
     drawBuzzPower(buzzPowers[0].path);
 }
@@ -70,13 +101,13 @@ function drawBuzzPower(path){
     fabric.Image.fromURL(path, img => {
 
         img.set({
-    left: 697 / 2,
-    top: 1016 / 2,
-    originX: "center",
-    originY: "center",
-    selectable: false,
-    evented: false
-});
+            left: 697 / 2,
+            top: 1016 / 2,
+            originX: "center",
+            originY: "center",
+            selectable: false,
+            evented: false
+        });
 
         img.layerType = "buzzPower";
 
@@ -86,4 +117,5 @@ function drawBuzzPower(path){
         sortLayers();
         canvas.renderAll();
     });
+
 }
