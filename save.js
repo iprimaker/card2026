38c3bc58dadab1d8467c55756e5db543c1800827
iprimaker <kinokonoshanv@gmail.com>
2026-07-09 06:33:29 +0900

    Update save.js

--- save.js
+++ save.js
@@ -2,33 +2,50 @@ import { getCanvas } from "./canvas.js";
 import { sortLayers } from "./layer.js";
 
 export function initSave(){
+
     const saveButton = document.getElementById("saveButton");
 
-    if(!saveButton) return;
+    const saveModal = document.getElementById("saveModal");
+    const confirmButton = document.getElementById("confirmSaveButton");
+    const cancelButton = document.getElementById("cancelSaveButton");
 
-    saveButton.addEventListener("click", saveImage);
+    saveButton.addEventListener("click", () => {
+        saveModal.classList.add("show");
+    });
+
+    cancelButton.addEventListener("click", () => {
+        saveModal.classList.remove("show");
+    });
+
+    confirmButton.addEventListener("click", () => {
+
+        saveModal.classList.remove("show");
+
+        saveImage();
+
+    });
+
 }
 
 function saveImage(){
 
     const canvas = getCanvas();
 
-    if(!canvas) return;
-
     sortLayers();
     canvas.discardActiveObject();
     canvas.renderAll();
 
     const image = canvas.toDataURL({
-        format: "png",
-        quality: 1,
-        multiplier: 3,
-        enableRetinaScaling: true
+        format:"png",
+        quality:1,
+        multiplier:3
     });
 
     const link = document.createElement("a");
+
     link.href = image;
     link.download = "original-card.png";
+
     link.click();
 
 }
