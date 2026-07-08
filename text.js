1cbc4e7a5eabfdce77ce053342f621db75d2acce
iprimaker <kinokonoshanv@gmail.com>
2026-07-09 07:06:20 +0900

    Update text.js

--- text.js
+++ text.js
@@ -186,7 +186,7 @@ export function initText(){
     }
 
     if(textVisible){
-        textVisible.checked = false;
+        textVisible.checked = true;
         textVisible.disabled = false;
     }
 
