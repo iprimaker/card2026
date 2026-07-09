a558d681f2fa272d1b00f996e034afc10fe7050d
iprimaker <kinokonoshanv@gmail.com>
2026-07-09 15:14:09 +0900

    Update image.js

--- image.js
+++ image.js
@@ -9,8 +9,8 @@ const CARD_WIDTH = 697;
 const CARD_HEIGHT = 1016;
 
 const BACKGROUND_LIST = [
-    { id: "background1", name: "ひみつ　A-☆2タイプ", path: "./backA1_1.png" },
-    { id: "background2", name: "ひみつ　A-☆3タイプ", path: "./backA1.png" },
+    { id: "background1", name: "ひみつ　　A-☆2タイプ", path: "./backA1_1.png" },
+    { id: "background2", name: "ひみつ　　A-☆3タイプ", path: "./backA1.png" },
     { id: "background3", name: "おねがい　Bタイプ", path: "./backB2.png" }
 ];
 
