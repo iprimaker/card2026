1419891f90cb3e65a0f696d5944a0589156bf0af
iprimaker <kinokonoshanv@gmail.com>
2026-07-11 01:43:13 +0900

    Update text.js

--- text.js
+++ text.js
@@ -390,7 +390,7 @@ export function initText(){
 
         // ひらがな・カタカナ・長音符・中黒・数字のみ許可
         costumeInput.value = costumeInput.value
-            .replace(/[^a-zA-Zぁ-ゖァ-ヶー0-9０-９ー・]/g, "")
+            .replace(/[^ぁ-ゖァ-ヶーA-Za-zＡ-Ｚａ-ｚ0-9０-９ー・\s]/g, "")
             .slice(0, 19);
 
         drawCostumeLetters(
