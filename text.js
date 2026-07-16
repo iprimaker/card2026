54d7369c3e67751786d1fe5bb727592b70c9f8d3
iprimaker <kinokonoshanv@gmail.com>
2026-07-17 01:52:41 +0900

    Update text.js

--- text.js
+++ text.js
@@ -291,7 +291,8 @@ const TEXT_STYLE = {
             frame2: createBTextStyle("#0EC6FF"),
             frame3: createBTextStyle("#008052"),
             frame4: createBTextStyle("#DF871F"),
-            frame5: createBTextStyle("#C91C27")
+            frame5: createBTextStyle("#C91C27"),
+            frame5: createBTextStyle("#5A0085")
         },
 
         /* ===========================
